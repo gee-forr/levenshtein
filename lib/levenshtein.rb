41e0c18a1ad1a0fd1a6f62212a45708c57190850
Gabriel Fortuna <gee.forr@gmail.com>
2012-11-22 15:44:30 +0200

    Realign data for better readability

--- lib/levenshtein.rb
+++ lib/levenshtein.rb
@@ -15,8 +15,8 @@ class Levenshtein
     return s2_size if s1_size == 0
     return s1_size if s2_size == 0
 
-    [ {strs: [first_string[1..s1_size-1], second_string], cost: 1},
-      {strs: [first_string, second_string[1..s2_size-1]], cost: 1},
+    [ {strs: [first_string[1..s1_size-1], second_string],               cost: 1},
+      {strs: [first_string,               second_string[1..s2_size-1]], cost: 1},
       {strs: [first_string[1..s1_size-1], second_string[1..s2_size-1]], cost: cost}
     ].map do |attempt|
       dist       = distance(attempt[:strs][0], attempt[:strs][1])
