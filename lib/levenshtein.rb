3f7744a773a6d354fae6bce38831711ca8a8dd2d
Gabriel Fortuna <gee.forr@gmail.com>
2012-11-22 15:43:16 +0200

    Complete memoisation

--- lib/levenshtein.rb
+++ lib/levenshtein.rb
@@ -5,7 +5,7 @@ class Levenshtein
 
   def self.distance(first_string, second_string)
     memo_key = "#{first_string}#{second_string}"
-    return @memo[memo_key] if @memo_key.has_key? memo_key 
+    return @memo[memo_key] if @memo.has_key? memo_key 
 
     cost    = 0
     cost    = 1 if first_string[0] != second_string[0]
@@ -15,10 +15,15 @@ class Levenshtein
     return s2_size if s1_size == 0
     return s1_size if s2_size == 0
 
-    [ # return the lowest value out of these three calls
-      distance(first_string[1..s1_size-1], second_string) + 1,
-      distance(first_string, second_string[1..s2_size-1]) + 1,
-      distance(first_string[1..s1_size-1], second_string[1..s2_size-1]) + cost
-    ].min
+    [ {strs: [first_string[1..s1_size-1], second_string], cost: 1},
+      {strs: [first_string, second_string[1..s2_size-1]], cost: 1},
+      {strs: [first_string[1..s1_size-1], second_string[1..s2_size-1]], cost: cost}
+    ].map do |attempt|
+      dist       = distance(attempt[:strs][0], attempt[:strs][1])
+      key        = "#{attempt[:strs][0]}#{attempt[:strs][1]}"
+      @memo[key] = dist
+
+      dist + attempt[:cost]
+    end.min
   end
 end
